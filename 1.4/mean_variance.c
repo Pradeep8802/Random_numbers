1506387f715463da5cd37c1a121b72414b1860ac
Pradeep8802 <102598014+Pradeep8802@users.noreply.github.com>
2022-07-02 06:35:12 +0530

    Create mean_variance.c

---
+++ 1.4/mean_variance.c
@@ -0,0 +1,54 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "coeffs.h"
+double var(char* str,int Mean)
+{
+int i=0,c;
+FILE *fp;
+double x, temp=0.0;
+
+fp = fopen(str,"r");
+//get numbers from file
+while(fscanf(fp,"%lf",&x)!=EOF)
+{
+//Count numbers in file
+i=i+1;
+//Add all numbers in file
+temp = temp+x*x;
+}
+fclose(fp);
+temp = temp/(i);
+return temp;
+}
+
+double mean1(char* str)
+{
+int i=0,c;
+FILE *fp;
+double x, temp=0.0;
+
+fp = fopen(str,"r");
+//get numbers from file
+while(fscanf(fp,"%lf",&x)!=EOF)
+{
+//Count numbers in file
+i=i+1;
+//Add all numbers in file
+temp = temp+x;
+}
+fclose(fp);
+temp = temp/(i);
+return temp;
+}
+
+int main(){
+
+    double Mean = mean1("uni.dat");
+    
+
+//Var(X)= E[X^2] - (E[X])^2 
+    double Variance=var("uni.dat",Mean)-Mean*Mean;
+printf("%f %f",Mean,Variance);
+}
