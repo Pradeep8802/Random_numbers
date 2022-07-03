753d3eef6d6f274c18e96e2b04de1dc188601e6e
Pradeep8802 <102598014+Pradeep8802@users.noreply.github.com>
2022-07-03 13:38:29 +0530

    Update and rename a to pdf_6.3.py

---
+++ 6.3/pdf_6.3.py
@@ -0,0 +1,61 @@
+#Importing numpy, scipy, mpmath and pyplot
+import numpy as np
+import mpmath as mp
+import scipy 
+import matplotlib.pyplot as plt
+import math
+
+#if using termux
+# import subprocess
+# import shlex
+#end if
+maxrange=50
+maxlim=6.0
+x = np.linspace(-maxlim,maxlim,maxrange)#points on the x axis
+z = np.linspace(-maxlim,maxlim,maxrange*50) #more points on the x axis
+simlen = int(1e6) #number of samples
+err = [] #declaring probability list
+pdf = [] #declaring pdf list
+h = 2*maxlim/(maxrange-1)
+#randvar = np.random.normal(0,1,simlen)
+#randvar = np.loadtxt('uni.dat',dtype='double')
+randvar = np.loadtxt('gau4.dat',dtype='double')
+
+for i in range(0,maxrange):
+    err_ind = np.nonzero(randvar < x[i]) #checking probability condition
+    err_n = np.size(err_ind) #computing the probability
+    err.append(err_n/simlen) #storing the probability values in a list
+
+    
+for i in range(0,maxrange-1):
+    test = (err[i+1]-err[i])/(x[i+1]-x[i])
+    pdf.append(test) #storing the pdf values in a list
+
+
+def pdf_Chi(x):
+    y=x* math.exp(-x*x/2)
+    if(x<0):
+        y=0
+    return y           
+
+    
+vect_pdf = np.vectorize(pdf_Chi, otypes=[float])
+
+plt.plot(x[0:(maxrange-1)].T,pdf, 'x')
+plt.plot(z,vect_pdf(z))
+plt.grid() #creating the grid
+plt.xlabel('$x$')
+plt.ylabel('$Y$')
+plt.legend(["Numerical","Theory"])
+
+#if using termux
+# plt.savefig('../figs/uni_pdf.pdf')
+#plt.savefig('../figs/uni_pdf.eps')
+#subprocess.run(shlex.split("termux-open ../figs/uni_pdf.pdf"))
+#if using termux
+# plt.savefig('../figs/gauss_pdf.pdf')
+# plt.savefig('../figs/gauss_pdf.eps')
+# subprocess.run(shlex.split("termux-open ../figs/gauss_pdf.pdf"))
+#else
+# plt.savefig('../figures/CDF_tri.png')
+plt.show() #opening the plot window
