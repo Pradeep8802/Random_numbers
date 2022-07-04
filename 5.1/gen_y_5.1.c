e38629d88256cfd19bf607ec7e9a14813dde797d
Pradeep8802 <102598014+Pradeep8802@users.noreply.github.com>
2022-07-04 12:34:37 +0530

    Update gen_y_5.1.c

--- 5.1/gen_y_5.1.c
+++ 5.1/gen_y_5.1.c
@@ -1,1 +1,14 @@
-a
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "5.h"
+
+int main(){
+uniform_Bernolli("ber5.dat", 1000000);
+gaussian("gau5.dat",1000000);
+int A=5;//A=5dB
+var_Y("ber5.dat","gau5.dat","Var_Y.dat",1000000,A);
+
+
+    return 0;
+}
