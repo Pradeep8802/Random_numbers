2a3e92adbc7de6ad2d0afe1b0e5f4e0c68375445
Pradeep8802 <102598014+Pradeep8802@users.noreply.github.com>
2022-07-04 17:14:22 +0530

    Update 4.1.c

--- 4.1/4.1.c
+++ 4.1/4.1.c
@@ -1,29 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-void triangular(char *str, int len)
-{
-int i,j;
-double temp;
-FILE *fp;
+#include "coeffs.h"
 
+void Uniformadd(char *str1,char *str2,char *str,int len)
+{
+uniform(str1, 1000000);
+uniform(str2, 1000000);
+int i=0,c;
+FILE *fp1;
+double x, temp=0.0;
+
+fp1 = fopen(str1,"r");
+
+FILE *fp2;
+double y;
+fp2 = fopen(str2,"r");
+
+FILE *fp;
 fp = fopen(str,"w");
-//Generate numbers
-for (i = 0; i < len; i++)
+
+//get numbers from file
+while(fscanf(fp1,"%lf",&x)!=EOF && (fscanf(fp2,"%lf",&y)!=EOF))
 {
-temp = 0;
-// for (j = 0; j < 12; j++)
-// {
-for (j = 0; j < 2; j++)
-{
-temp += (double)rand()/RAND_MAX;
-}
-//temp-=6;
+//Count numbers in file
+i=i+1;
+//Add all numbers in file
+temp = x+y;
 fprintf(fp,"%lf\n",temp);
 }
+fclose(fp1);
+fclose(fp2);
 fclose(fp);
-
 }
+
+
 int main(){
-triangular("fourth1.dat", 1000000);
+
+Uniformadd("uni1.dat","uni2.dat", "fourth.dat",1000000);
 return 0;}
