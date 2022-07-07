758e8a91bf9cdba4a88cb01cebfab6993fb18081
Pradeep8802 <102598014+Pradeep8802@users.noreply.github.com>
2022-07-07 23:04:46 +0530

    Update cdf_4.2.py

--- 4.4/cdf_4.2.py
+++ 4.4/cdf_4.2.py
@@ -2,42 +2,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# #if using termux
-# import subprocess
-# import shlex
-# #end if
+#if using termux
+import subprocess
+import shlex
+#end if
+
+
 
 x = np.linspace(-4,4,30)#points on the x axis
 simlen = int(1e6) #number of samples
 err = [] #declaring probability list
 #randvar = np.random.normal(0,1,simlen)
 randvar = np.loadtxt('fourth1.dat',dtype='double')
-
+#randvar = np.loadtxt('gau.dat',dtype='double')
 for i in range(0,30):
-    err_ind = np.nonzero(randvar < x[i]) #checking probability condition
-    err_n = np.size(err_ind) #computing the probability
-    err.append(err_n/simlen) #storing the probability values in a list
+	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
+	err_n = np.size(err_ind) #computing the probability
+	err.append(err_n/simlen) #storing the probability values in a list
 
-
-
-plt.plot(x,err,'x')
-
+	
+def cdf_tri(x):
+    if (x<=0):
+       return 0
+    elif (x>=0 and x <1):
+       return (x*x)/2
+    elif (x>=1 and x <2):
+       return (2*x-(x*x)/2 -1)
+    else:
+       return 1
+    
+theoritical = []
+for i in range(len(x)):
+   theoritical.append(cdf_tri(x[i]))
+plt.plot(x,err,'x')#plotting the CDF
+plt.plot(x,theoritical)
 plt.grid() #creating the grid
-plt.xlabel('$x_i$')
-plt.ylabel('$F_X(x_i)$')
-#plt.legend(["Numerical","Theory"])
+plt.xlabel('$x$')
+plt.ylabel('$F_X(x)$')
 
-# #if using termux
-# plt.savefig('../figs/uni_cdf.pdf')
-# plt.savefig('../figs/uni_cdf.eps')
-# subprocess.run(shlex.split("termux-open ../figs/uni_cdf.pdf"))
-#if using termux
-#plt.savefig('../figs/gauss_cdf.pdf')
-#plt.savefig('../figs/gauss_cdf.eps')
-#subprocess.run(shlex.split("termux-open ../figs/gauss_cdf.pdf"))
-#else
 plt.show() #opening the plot window
-
 
 
 
