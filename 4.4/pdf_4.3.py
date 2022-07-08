8615b29fb006eb7cdfa42a29a80affeb872c83b1
Pradeep8802 <102598014+Pradeep8802@users.noreply.github.com>
2022-07-08 11:34:10 +0530

    Update pdf_4.3.py

--- 4.4/pdf_4.3.py
+++ 4.4/pdf_4.3.py
@@ -3,16 +3,16 @@ import numpy as np
 import mpmath as mp
 import scipy 
 import matplotlib.pyplot as plt
-import math
 
 #if using termux
-# import subprocess
-# import shlex
+import subprocess
+import shlex
 #end if
-maxrange=50
+
+
+maxrange=80
 maxlim=6.0
 x = np.linspace(-maxlim,maxlim,maxrange)#points on the x axis
-xx = np.linspace(-maxlim,maxlim,maxrange*50) #more points on the x axis
 simlen = int(1e6) #number of samples
 err = [] #declaring probability list
 pdf = [] #declaring pdf list
@@ -22,113 +22,43 @@ h = 2*maxlim/(maxrange-1)
 randvar = np.loadtxt('fourth1.dat',dtype='double')
 
 for i in range(0,maxrange):
-    err_ind = np.nonzero(randvar < x[i]) #checking probability condition
-    err_n = np.size(err_ind) #computing the probability
-    err.append(err_n/simlen) #storing the probability values in a list
+	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
+	err_n = np.size(err_ind) #computing the probability
+	err.append(err_n/simlen) #storing the probability values in a list
 
-    
+	
 for i in range(0,maxrange-1):
-    test = (err[i+1]-err[i])/(x[i+1]-x[i])
-    pdf.append(test) #storing the pdf values in a list
+	test = (err[i+1]-err[i])/(x[i+1]-x[i])
+	pdf.append(test) #storing the pdf values in a list
 
+def tri_pdf(x):
+    if x<0:
+        return 1e-5
+    elif x>=0 and x<=1:
+        return x
+    elif x>1 and x<=2:
+        return 2-x
+    else :
+        return 0            
+vec_tri_pdf = scipy.vectorize(tri_pdf)
 
-def pdf_traingular(x):
-    y=x
-    if(x<0):
-        y=0
-    elif(x>2):
-        y=0
-    elif(x>1 and x<2):
-        y=2-x 
-    return y           
+plt.plot(x[0:(maxrange-1)].T,pdf,'x')#numerical
+plt.plot(x,vec_tri_pdf(x))# Theory
 
-    
-vect_pdf = np.vectorize(pdf_traingular, otypes=[np.float])
-
-plt.plot(x[0:(maxrange-1)].T,pdf, 'x')
-plt.plot(xx,vect_pdf(xx))
 plt.grid() #creating the grid
-plt.xlabel('$x$')
-plt.ylabel('$Y$')
+plt.xlabel('$x_i$')
+plt.ylabel('$p_T(x_i)$')
 plt.legend(["Numerical","Theory"])
 
+
 #if using termux
-# plt.savefig('../figs/uni_pdf.pdf')
+#plt.savefig('../figs/uni_pdf.pdf')
 #plt.savefig('../figs/uni_pdf.eps')
 #subprocess.run(shlex.split("termux-open ../figs/uni_pdf.pdf"))
 #if using termux
-# plt.savefig('../figs/gauss_pdf.pdf')
-# plt.savefig('../figs/gauss_pdf.eps')
-# subprocess.run(shlex.split("termux-open ../figs/gauss_pdf.pdf"))
+plt.show()
+
 #else
-# plt.savefig('../figures/CDF_tri.png')
-plt.show() #opening the plot window
+#plt.show() #opening the plot window
 
-# #Importing numpy, scipy, mpmath and pyplot
-# import numpy as np
-# import mpmath as mp
-# import scipy 
-# import matplotlib.pyplot as plt
-
-# # #if using termux
-# # import subprocess
-# # import shlex
-# # #end if
-
-
-# maxrange=50
-# maxlim=6.0
-# x = np.linspace(-maxlim,maxlim,maxrange)#points on the x axis
-# simlen = int(1e6) #number of samples
-# err = [] #declaring probability list
-
-# h = 2*maxlim/(maxrange-1)
-# #randvar = np.random.normal(0,1,simlen)
-# #randvar = np.loadtxt('uni.dat',dtype='double')
-# randvar = np.loadtxt('fourth1.dat',dtype='double')
-
-# for i in range(0,maxrange):
-#     err_ind = np.nonzero(randvar < x[i]) #checking probability condition
-#     err_n = np.size(err_ind) #computing the probability
-#     err.append(err_n/simlen) #storing the probability values in a list
-
-# pdf = [] #Declaring pdf as a list
-# for i in range(0,maxrange-1):
-#     new_dat=(err[i+1]-err[i])/(x[i+1]-x[i])
-#     pdf.append(new_dat) # Storing the pdf values in the list pdf created before 
-
-# def pdf_traingular(x):
-#     y=[]
-#     for i in range(50):
-#         if(x[i]>=0 and x[i]<1):
-#             y.append(x)
-#         elif(x[i]>=1 and x[i]<2):
-#             y.append(2-x)
-#         elif(x[i]<0 or x[i]>2):
-#             y.append(0)                             
-#     return y
-# print(x)    
-# vect_pdf= scipy.vectorize(pdf_traingular)
-
-# plt.plot(x[0:(maxrange-1)].T,pdf,'x')
-# plt.plot(x,vect_pdf(x))#plotting the CDF
-# plt.grid() #creating the grid
-# plt.xlabel('$x_i$')
-# plt.ylabel('$p_X(x_i)$')
-# plt.legend(["Numerical","Theory"])
-
-# #if using termux
-# #plt.savefig('../figs/uni_pdf.pdf')
-# #plt.savefig('../figs/uni_pdf.eps')
-# #subprocess.run(shlex.split("termux-open ../figs/uni_pdf.pdf"))
-# #if using termux
-# #plt.savefig('../figs/gauss_pdf.pdf')
-# #plt.savefig('../figs/gauss_pdf.eps')
-# #subprocess.run(shlex.split("termux-open ../figs/gauss_pdf.pdf"))
-# #else
-# plt.show() #opening the plot window
-
-
-# # Two dices (rectangular) p = 1/6 use convolution
-# #use built in
 
